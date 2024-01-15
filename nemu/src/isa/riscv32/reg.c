8f60bac4663b284cf29089d35c5c7bf0b2cdfdb3
tracer-ics2023 <tracer@njuics.org>
2024-01-14 19:42:49 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:42:49 up  1:01,  0 users,  load average: 0.27, 0.19, 0.15

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,7 +26,7 @@ const char *regs[] = {
 void isa_reg_display() {
   for (int i = 0; i < 32; i++)
   {
-    printf("%.16s%.16x%.16d\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
+    printf("%.16s %.16x %.16d\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
   }
 }
 
