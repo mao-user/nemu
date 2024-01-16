4cba1ff6955a22cde079526660d9192233afdc84
tracer-ics2023 <tracer@njuics.org>
2024-01-15 22:06:01 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:01 up 15:54,  1 user,  load average: 0.12, 0.15, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -76,12 +76,12 @@ static int cmd_x(char *args) {
 
   int i, j;
   for (i = 0; i < n;) {
-    printf("%#018x: ",expr);
+    printf("%#010x: ",expr);
     
     for (j = 0; i < n && j < 4; i++, j++) {
       word_t w = vaddr_read(expr, 1);
       expr += 8;
-      printf("%#018x ", w);
+      printf("%#010x ", w);
     }
     puts("");
   }
