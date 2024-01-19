193892b8a94a4f4f4ee52de0d68e386e36f20535
tracer-ics2023 <tracer@njuics.org>
2024-01-18 21:40:25 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:25 up  4:36,  0 users,  load average: 0.15, 0.14, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,3 +1,18 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
 #include <isa.h>
 
 /* We use the POSIX regex functions to process regular expressions.
@@ -5,18 +20,17 @@
  */
 #include <regex.h>
 
-#include "memory/paddr.h"
 
 enum {
-  TK_NOTYPE = 256, TK_EQ=255, NUMBER=254,negative=253,
-  sixteen=252,reg=251,deref=250,and=249,neq=248,PC=257,
-
-  /* TODO: Add more token types */
-
+  TK_NOTYPE = 256, 
+  TK_EQ,
+  TK_NUM, // 10 & 16
+  TK_REG,
+  TK_VAR,
 };
 
 static struct rule {
-  char *regex;
+  const char *regex;
   int token_type;
 } rules[] = {
 
@@ -24,28 +38,24 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {"0x[0-9a-f]+",sixteen},
-  {" +", TK_NOTYPE},    //spaces
-  {"\\+", '+'},         //plus
-  {"==", TK_EQ},        //equal
-  {"\\-", '-'},         //sub
-  {"\\*",'*'},          //mul
-  {"/",'/'},            //div
-  {"[0-9]+",NUMBER},    
-  {"\\(",'('},          
-  {"\\)",')'},          
-  {"\\$\\p\\c",PC},
-  {"\\$[a-z]+",reg},
-  {"&&",and},
-  {"!=",neq},
-  
+  {" +", TK_NOTYPE},    // spaces
+  {"\\+", '+'},         // plus
+  {"-", '-'},
+  {"\\*", '*'},
+  {"/", '/'},
+  {"==", TK_EQ},        // equal
+  {"\\(", '('},
+  {"\\)", ')'},
+
+  {"[0-9]+", TK_NUM}, // TODO: non-capture notation (?:pattern) makes compilation failed
+  {"\\$\\w+", TK_REG},
+  {"[A-Za-z_]\\w*", TK_VAR},
+
 };
 
-#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+#define NR_REGEX ARRLEN(rules)
 
 static regex_t re[NR_REGEX] = {};
-
-unsigned int calculate();
 
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
@@ -69,7 +79,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[500] __attribute__((used)) = {};
+static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
@@ -78,32 +88,36 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
-  memset(tokens,0,sizeof(tokens));
-
-  while (e[position] != '\0' && e[position]!='\n') {
+  while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position; 
+      int reg_res = regexec(&re[i], e + position, 1, &pmatch, 0);
+      if (reg_res == 0 && pmatch.rm_so == 0) {
+        char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
+
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        
         position += substr_len;
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
+        
+        if (rules[i].token_type == TK_NOTYPE) break;
+
+        tokens[nr_token].type = rules[i].token_type;
         switch (rules[i].token_type) {
-          case 256: break;
-          default: {
-            tokens[nr_token].type=rules[i].token_type;
-            strncpy(tokens[nr_token].str,substr_start,substr_len);
-            nr_token++;
-            break;
-          }
+          case TK_NUM:
+          case TK_REG:
+          case TK_VAR:
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            // todo: handle overflow (token exceeding size of 32B)
         }
+        nr_token++;
+
         break;
       }
     }
-    // something wrong happend
+
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
@@ -114,329 +128,99 @@ static bool make_token(char *e) {
 }
 
 
-word_t expr(char *e, bool *if_success) {
+bool check_parentheses(int p, int q) {
+  if (tokens[p].type=='(' && tokens[q].type==')') {
+    int par = 0;
+    for (int i = p; i <= q; i++) {
+      if (tokens[i].type=='(') par++;
+      else if (tokens[i].type==')') par--;
+
+      if (par == 0) return i==q; // the leftest parenthese is matched
+    }
+  }
+  return false;
+}
+
+int find_major(int p, int q) {
+  int ret = -1, par = 0, op_type = 0;
+  for (int i = p; i <= q; i++) {
+    if (tokens[i].type == TK_NUM) {
+      continue;
+    }
+    if (tokens[i].type == '(') {
+      par++;
+    } else if (tokens[i].type == ')') {
+      if (par == 0) {
+        return -1;
+      }
+      par--;
+    } else if (par > 0) {
+      continue;
+    } else {
+      int tmp_type = 0;
+      switch (tokens[i].type) {
+      case '*': case '/': tmp_type = 1; break;
+      case '+': case '-': tmp_type = 2; break;
+      default: assert(0);
+      }
+      if (tmp_type >= op_type) {
+        op_type = tmp_type;
+        ret = i;
+      }
+    }
+  }
+  if (par != 0) return -1;
+  return ret;
+}
+
+
+word_t eval(int p, int q, bool *ok) {
+  *ok = true;
+  if (p > q) {
+    *ok = false;
+    return 0;
+  } else if (p == q) {
+    if (tokens[p].type != TK_NUM) {
+      *ok = false;
+      return 0;
+    }
+    word_t ret = strtol(tokens[p].str, NULL, 10);
+    return ret;
+  } else if (check_parentheses(p, q)) {
+    return eval(p+1, q-1, ok);
+  } else {    
+    int major = find_major(p, q);
+    if (major < 0) {
+      *ok = false;
+      return 0;
+    }
+
+    word_t val1 = eval(p, major-1, ok);
+    if (!*ok) return 0;
+    word_t val2 = eval(major+1, q, ok);
+    if (!*ok) return 0;
+    
+    switch(tokens[major].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': if (val2 == 0) {
+        *ok = false;
+        return 0;
+      } 
+      return (sword_t)val1 / (sword_t)val2; // e.g. -1/2, may not pass the expr test
+      default: assert(0);
+    }
+  }
+}
+
+
+word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
-    *if_success = false;
+    *success = false;
     return 0;
   }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  for(int i = 0;i < nr_token;i++){
-    //negative number
-    if(tokens[i].type=='-'&&(i==0||(tokens[i-1].type!=NUMBER&&tokens[i-1].type!=')'))){
-      tokens[i].type=negative;
-      tokens[i].str[0]='-';
-    }
-    //dereference
-    if(tokens[i].type=='*'&& (i == 0 || (tokens[i - 1].type != NUMBER &&tokens[i-1].type!=')')) )
-      tokens[i].type=deref;
-  }
-
-  unsigned int ans=calculate(0,nr_token);
-  *if_success = true;
-  return ans;
+  
+  return eval(0, nr_token-1, success);
 }
 
-int check_(int count,int q){
-  int tmp=0;
-  for(int i = count;i < q;i ++ ){
-    if(tokens[i].type=='(')
-      tmp+=1;
-    else if(tokens[i].type==')')
-      tmp-=1;
-    if(tmp==0){
-      tmp=i;
-      break;
-    }
-  }
-  return tmp;
-}
-
-unsigned int calculate(int p,int q){
-  if(p >= q){
-    printf("wrong");
-    return 0;
-  }
-  int count=p,num_of_stack=0,sign[100]={0},tmp=0,i=0;
-  unsigned int temp1=0,temp2=0,temp=0,stack[100]={0},val1=0,val2=0;
-  bool flag=true;
-  memset(sign,0,sizeof(sign));
-  memset(stack,0,sizeof(stack));
-  while(count < q){
-    count++;
-    switch(tokens[count-1].type){
-      case 257:
-          stack[num_of_stack++]=cpu.pc;
-          break;
-      case 253:
-          tmp=0;
-          for(i = count;i < q;i ++ ){
-            if(tokens[i].type=='(')
-              tmp+=1;
-            else if(tokens[i].type==')')
-              tmp-=1;
-            if(tmp==0&&(tokens[i].type==NUMBER||tokens[i].type==')')){
-              tmp=i;
-              break;
-            }
-          }
-          temp=(-1U)*calculate(count,tmp+1);
-          tokens[tmp].type=NUMBER;
-          sprintf(tokens[tmp].str,"%u",temp);
-          count=tmp;
-          break;
-      case 255:
-          tmp=check_(count,q);
-          if(i == q)
-            val2=calculate(count,q);
-          else 
-            val2=calculate(count,tmp+1);
-          val1=stack[num_of_stack-1];
-          stack[num_of_stack-1]=0;
-          if(val1==val2)
-            temp=1;
-          else 
-            temp=0;
-          tokens[tmp].type=NUMBER;
-          sprintf(tokens[tmp].str,"%u",temp);
-          count=tmp;
-          num_of_stack--;
-          break;
-      case 248:
-          tmp=check_(count,q);
-          if(i == q)
-            val2=calculate(count,q);
-          else 
-            val2=calculate(count,tmp+1);
-          val1=stack[num_of_stack-1];
-          stack[num_of_stack-1]=0;
-          if(val1==val2)
-            temp=0;
-          else 
-            temp=1;
-          tokens[tmp].type=NUMBER;
-          sprintf(tokens[tmp].str,"%u",temp);
-          count=tmp;
-          num_of_stack--;
-          break;
-      case 249:
-          tmp=check_(count,q);
-          if(i == q)
-            val2=calculate(count,q);
-          else 
-            val2=calculate(count,tmp+1);
-          val1=stack[num_of_stack-1];
-          stack[num_of_stack-1]=0;
-          temp=val1&&val2;
-          tokens[tmp].type=NUMBER;
-          sprintf(tokens[tmp].str,"%u",temp);
-          count=tmp;
-          num_of_stack--;
-          break;
-      case 251:
-          flag=true;
-          stack[num_of_stack++]=isa_reg_str2val(tokens[count-1].str,&flag);
-          break;
-      case 250:
-          tmp=check_(count,q);
-          if(i == q) 
-            temp=calculate(count,q);
-          else 
-            temp=calculate(count,tmp+1);
-          tokens[tmp].type=NUMBER;
-          sprintf(tokens[tmp].str,"%u",temp);
-          count=tmp;
-          break;
-      case 256:
-          break;
-      case '+':
-          stack[num_of_stack++]='+';
-          sign[num_of_stack-1]='+';
-          break;
-      case '-':
-          stack[num_of_stack++]='-';
-          sign[num_of_stack-1]='-';
-          break; 
-      case '*':
-          stack[num_of_stack++]='*';
-          sign[num_of_stack-1]='*';
-          break;
-      case '/':
-          stack[num_of_stack++]='/';
-          sign[num_of_stack-1]='/';
-          break;
-      case 252:
-          if(num_of_stack == 0)  {
-              sscanf(tokens[count-1].str,"%x", &temp);
-              stack[num_of_stack++]=temp;
-              break;
-            }
-            num_of_stack-=1;
-          if(sign[num_of_stack]=='*'||sign[num_of_stack]=='/'){
-            temp1=stack[num_of_stack-1];
-            sscanf(tokens[count-1].str,"%x", &temp2);
-            if(sign[num_of_stack]=='*')
-              temp1=temp1*temp2;
-            else{
-              if(temp2==0){
-                printf("divided by 0\n");
-                return 0;
-              }
-              temp1=temp1/temp2;
-            }
-            stack[num_of_stack-1]=temp1;
-            sign[num_of_stack]=0;
-            stack[num_of_stack]=0;
-          }
-          else{
-            sscanf(tokens[count-1].str,"%u", &temp);
-            stack[num_of_stack+1]=temp;
-            num_of_stack+=2;
-          }
-          break;
-      case 254:
-          if(num_of_stack == 0)  {
-              sscanf(tokens[count-1].str,"%u", &temp);
-              stack[num_of_stack++]=temp;
-              break;
-            }
-            num_of_stack-=1;
-          if(sign[num_of_stack]=='*'||sign[num_of_stack]=='/'){
-            temp1=stack[num_of_stack-1];
-            sscanf(tokens[count-1].str,"%u", &temp2);
-            if(sign[num_of_stack]=='*')
-              temp1=temp1*temp2;
-            else{
-              if(temp2==0){
-                printf("divided by 0\n");
-                return 0;
-              }
-              temp1=temp1/temp2;
-            }
-            stack[num_of_stack-1]=temp1;
-            sign[num_of_stack]=0;
-            stack[num_of_stack]=0;
-          }
-          else{
-            sscanf(tokens[count-1].str,"%u", &temp);
-            stack[num_of_stack+1]=temp;
-            num_of_stack+=2;
-          }
-          break;
-      case '(':
-          stack[num_of_stack++]='(';
-          sign[num_of_stack-1]='(';
-          break;
-      case ')':
-          temp=0;
-          num_of_stack-=1;
-          while(sign[num_of_stack]!='('){
-            switch(sign[num_of_stack]){
-              case '-':
-                if(stack[num_of_stack-2]!='(' && sign[num_of_stack-2]==sign[num_of_stack]){
-                  temp=stack[num_of_stack-1]+temp;
-                  sign[num_of_stack]=0;
-                  stack[num_of_stack]=0;
-                  stack[num_of_stack-1]=temp;
-                }     
-                else if(stack[num_of_stack-2]!='('){
-                  temp=stack[num_of_stack-1]-temp;
-                  sign[num_of_stack]=0;
-                  stack[num_of_stack]=0;
-                  stack[num_of_stack-1]=temp;
-                }
-                else{
-                  temp=stack[num_of_stack-1]-temp;
-                  stack[num_of_stack-1]=temp;
-                }
-                sign[num_of_stack]=0;
-                num_of_stack--;
-                break;
-              case '+':
-                if(stack[num_of_stack-2]!='(' && sign[num_of_stack-2]==sign[num_of_stack]){
-                  temp=stack[num_of_stack-1]+temp;
-                  sign[num_of_stack]=0;
-                  stack[num_of_stack]=0;
-                  stack[num_of_stack-1]=temp;
-                }
-                else if(stack[num_of_stack-2]!='('){
-                  temp=stack[num_of_stack-1]-temp;
-                  sign[num_of_stack]=0;
-                  stack[num_of_stack]=0;
-                  stack[num_of_stack-1]=temp;
-                }
-                else{
-                  temp=stack[num_of_stack-1]+temp;
-                  stack[num_of_stack-1]=temp;
-                }
-                sign[num_of_stack]=0;
-                num_of_stack--;
-                break;
-              default:
-                temp=stack[num_of_stack];
-                stack[num_of_stack]=0;
-                num_of_stack--;
-            }
-          }
-          sign[num_of_stack]=0;
-          count-=1;
-          tokens[count].type=254;
-          sprintf(tokens[count].str,"%u",temp); 
-          break;   
-      default:
-          assert(0);
-    }
-  } 
-  /*
-  *we have pushed all the vals into the stack.wo also dealt with the nums
-  *of the call sign(translated by Bing)
-  */
-  temp=stack[--num_of_stack];
-  while( num_of_stack > 0){
-    switch(sign[num_of_stack]){
-      case '-':
-        if(num_of_stack > 2 && sign[num_of_stack-2]==sign[num_of_stack]){
-          temp=stack[num_of_stack-1]+temp;
-          sign[num_of_stack]=0;
-          stack[num_of_stack]=0;
-          stack[num_of_stack-1]=temp;
-        }
-        else if(num_of_stack >2){
-          temp=stack[num_of_stack-1]-temp;
-          sign[num_of_stack]=0;
-          stack[num_of_stack]=0;
-          stack[num_of_stack-1]=temp;
-        }
-        else{
-          temp=stack[num_of_stack-1]-temp;
-          stack[num_of_stack-1]=temp;
-        }
-        num_of_stack--;
-        break;
-      case '+':
-        if(num_of_stack > 2 && sign[num_of_stack-2]==sign[num_of_stack]){
-          temp=stack[num_of_stack-1]+temp;
-          sign[num_of_stack]=0;
-          stack[num_of_stack]=0;
-          stack[num_of_stack-1]=temp;
-        }
-        else if(num_of_stack >2){
-          temp=stack[num_of_stack-1]-temp;
-          sign[num_of_stack]=0;
-          stack[num_of_stack]=0;
-          stack[num_of_stack-1]=temp;
-        }
-        else{
-          temp=stack[num_of_stack-1]+temp;
-          stack[num_of_stack-1]=temp;
-        }
-        num_of_stack--;
-        break;
-      default:
-        temp=stack[num_of_stack];
-        stack[num_of_stack]=0;
-        num_of_stack--;
-    }
-  }
-  return temp;
-}
--- nemu/src/monitor/sdb/watchpoint.h
+++
@@ -1,8 +0,0 @@
-#ifndef __WATCHPOINT_H__
-#define __WATCHPOINT_H__
-
-#include <common.h>
-
-typedef struct watchpoint {} WP;
-
-#endif
