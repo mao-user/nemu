8abab0cf2e5bb05b613108d76ed43e6432b21bd1
tracer-ics2023 <tracer@njuics.org>
2024-01-18 20:02:27 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:02:27 up  2:58,  0 users,  load average: 0.27, 0.20, 0.18

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -13,31 +13,120 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "sdb.h"
+#include <isa.h>
 
 #define NR_WP 32
+
+word_t expr(char *e, bool *success);
 
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  char exp[128];
+  uint32_t last;
   /* TODO: Add more members if necessary */
-
 } WP;
 
 static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
+static WP *head = NULL, *free_ = NULL, *curr = NULL;
 
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    strcpy(wp_pool[i].exp, "\0");
+    wp_pool[i].last = 0;
   }
 
   head = NULL;
   free_ = wp_pool;
 }
-
+WP* return_(){
+  return head;
+}
+WP* search_(WP *wp){
+  WP* temp=head;
+  while(temp){
+    if(temp->next==wp) break;
+    temp=temp->next;
+  }
+  return temp;
+}
+WP* new_wp(char *expr){
+  if(head==NULL){
+    head=free_;
+    free_=free_->next;
+    head->next=NULL;
+    strcpy(head->exp,expr);
+    curr=head;
+  }
+  else{
+    if(free_==NULL) 
+      return NULL;
+    else{
+      curr->next=free_;
+      free_=free_->next;
+      curr=curr->next;
+      curr->next=NULL;
+      strcpy(curr->exp,expr);
+    }
+  }
+  return curr;
+}
+void free_wp(int des){
+  WP *wp=head;
+  while(wp!=NULL){
+    if(wp->NO==des) break;
+    else wp=wp->next;
+  }
+  if(wp==head){
+    head=head->next;
+    wp->next=free_;
+    free_=wp;
+  }
+  else if(wp==curr){
+    wp->next=free_;
+    free_=wp;
+    curr=search_(wp);
+    curr->next=NULL;
+  }
+  else{
+    search_(wp)->next=wp->next;
+    wp->next=free_;
+    free_=wp;
+  }
+};
 /* TODO: Implement the functionality of watchpoint */
-
+void check_wp(){
+  bool success;
+  WP *itr = head;
+  while(itr != NULL){
+    uint32_t res = expr(itr->exp,&success);
+    if(!success){
+      printf("The expression of watch point %d is invalid!\n",itr->NO);
+    }
+    else if(res != itr->last){
+      printf("Num:%-6d\t Expr:%-20s\t  New Val:%-14u\t  Old Val:%-14u\n",
+        itr->NO,
+        itr->exp,
+        res,
+        itr->last);
+      itr->last = res;
+      nemu_state.state = NEMU_STOP;
+    }
+    itr=itr->next;
+  }
+}
+void print_wp(){
+  WP *itr = head;
+  if(head==NULL) {
+    printf("there is no watchpoint in the pool");
+    return;
+  }
+  printf("%-15s%-7s%s\n","NO","Exp","Val");
+  while(itr != NULL){
+    printf("%02d\t%10s\t%-10u\n",itr->NO,itr->exp,itr->last);
+    itr = itr->next;
+  }
+}
---
+++ nemu/src/monitor/sdb/watchpoint.h
@@ -0,0 +1,8 @@
+#ifndef __WATCHPOINT_H__
+#define __WATCHPOINT_H__
+
+#include <common.h>
+
+typedef struct watchpoint {} WP;
+
+#endif
