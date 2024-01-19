d4924889808bdfc2da9543a6316195ccee4e202b
tracer-ics2023 <tracer@njuics.org>
2024-01-18 17:40:06 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:40:06 up 36 min,  0 users,  load average: 0.13, 0.17, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -38,7 +38,7 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {"\\s", TK_NOTYPE},    // spaces
+  {" ", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"-", '-'},
   {"\\*", '*'},
