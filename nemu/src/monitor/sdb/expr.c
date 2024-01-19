d0284aa82d324443610c787690ccc0c4b2300eb1
tracer-ics2023 <tracer@njuics.org>
2024-01-18 18:02:32 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:32 up 58 min,  0 users,  load average: 0.15, 0.15, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -101,7 +101,7 @@ static bool make_token(char *e) {
         
         position += substr_len;
         
-        if (rules[i].token_type == TK_NOTYPE) break;
+        if (rules[i].token_type == TK_NOTYPE) continue;
 
         tokens[nr_token].type = rules[i].token_type;
         switch (rules[i].token_type) {
