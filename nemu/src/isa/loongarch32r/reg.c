aff6cd1cec4c5e325601a500bb9fd6165e7b0a6e
tracer-ics2023 <tracer@njuics.org>
2024-01-28 21:31:20 -0700

    >  compile NEMU 221220000 张三 Linux kun-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:20 up 13 min,  0 users,  load average: 0.17, 0.18, 0.14

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -16,16 +16,36 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
+#define NR_REGS ARRLEN(regs) //sizeof(regs)/sizeof(regs[0])
+
+
 const char *regs[] = {
-  "$0", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
-  "a4", "a5", "a6", "a7", "t0", "t1", "t2", "t3",
-  "t4", "t5", "t6", "t7", "t8", "rs", "fp", "s0",
-  "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+
 void isa_reg_display() {
+  printf("%-15s%-17s%-15s\n", "Registers", "Hexadecimal", "Decimal");
+  for(int i = 0; i < NR_REGS; ++i){
+    printf("%-15s0x%-15x%-15d\n",regs[i], cpu.gpr[i], cpu.gpr[i]);
+  }
+  printf("%-15s0x%-15x%-15d\n","pc", cpu.pc, cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  *success = false;
+  for(int i = 0; i < NR_REGS; ++i ){
+    if(strcmp(regs[i], s+1) == 0){ 
+      *success = true; 
+      return cpu.gpr[i];
+    }
+  }
+  if(strcmp("pc", s+1) == 0){
+    *success = true; 
+    return cpu.pc;
+  }
   return 0;
 }
